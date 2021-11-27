1010465b568315771e29ecaa1dbcc77e120d611d
Marcos Paulo Marques Correa Gomes <t2m.mpgomes@terceiros.mag.com.br>
2021-11-27 12:20:30 -0300

    code: update

---
+++ Course/Course/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization; //para o uso do CultureInfo
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double x = 10.35784;
+            int y = 32;
+            string z = "Maria";
+            char w = 'F';
+
+            Console.Write("Olá mundo!");
+            Console.WriteLine("Bom dia!");
+            Console.WriteLine("Até mais.");
+
+            Console.WriteLine();
+            Console.WriteLine(x);
+            Console.WriteLine(x.ToString("F2")); //o F define o numero de casas decimais
+            Console.WriteLine(x.ToString("F4"));
+            Console.WriteLine(x.ToString("F2", CultureInfo.InvariantCulture)); //desconsidera formatacao de pais (coloca . como separador)
+
+            Console.WriteLine();
+            Console.WriteLine("RESULTADO = " + x);
+            Console.WriteLine("O valor do troco é " + x + " reais");
+            Console.WriteLine("O valor do troco é " + x.ToString("F2") + " reais");
+
+            Console.WriteLine();
+            Console.WriteLine("A paciente " + z + " tem " + y + " anos e seu sexo é: " + w);
+
+            Console.ReadLine();
+        }
+    }
+}
