bc45b2910d653cdbcd5f7604ab8810ef45c565f6
Marcos Paulo Marques Corrêa <marcos_barker@hotmail.com>
2021-12-03 20:31:13 -0300

    code: update

---
+++ exenProjsDotNet/NotasAluno/NotasAluno/Aluno.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotasAluno
+{
+    class Aluno
+    {
+        public double NotaA;
+        public double NotaB;
+        public double NotaC;
+        public string NomeAluno;
+
+        public double NotaFinal()
+        {
+            return NotaA + NotaB + NotaC;
+        }
+
+        public string Situacao()
+        {
+            if (NotaFinal() >= 60)
+            {
+                return "\nAPROVADO";
+            }
+            else
+            {
+                double falta = 60 - NotaFinal();
+                return "\nREPROVADO\nFALTAM " + falta + " PONTOS";
+            }
+        }
+
+        public override string ToString()
+        {
+            return "NOTA FINAL = " + NotaFinal().ToString("F2") + " " + Situacao();
+        }
+    }
+}
---
+++ exenProjsDotNet/NotasAluno/NotasAluno/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace NotasAluno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno = new Aluno();
+
+            Console.Write("Nome do aluno: ");
+            aluno.NomeAluno = Console.ReadLine();
+
+            Console.WriteLine("Digite tres notas do aluno: ");
+            aluno.NotaA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.NotaB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.NotaC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine(aluno);
+        }
+    }
+}
