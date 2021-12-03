21290a198a18683471f6231d47b5325faab11ff9
Marcos Paulo Marques Corrêa <marcos_barker@hotmail.com>
2021-12-03 18:12:25 -0300

    code: update

---
+++ exenProjsDotNet/LarguraAlturaRetangulo/LarguraAlturaTreiangulo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LarguraAlturaTreiangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo r = new Retangulo();
+
+            Console.WriteLine("Entre com a largura e altura do retangulo:");
+            Console.Write("Largura: ");
+            r.Largura = double.Parse(Console.ReadLine());
+
+            Console.Write("Altura: ");
+            r.Altura = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(r);
+        }
+    }
+}
---
+++ exenProjsDotNet/LarguraAlturaRetangulo/LarguraAlturaTreiangulo/Retangulo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace LarguraAlturaTreiangulo
+{
+    class Retangulo
+    {
+        public double Largura;
+        public double Altura;
+
+        public double Area()
+        {
+            return Largura * Altura;
+        }
+
+        public double Perimetro()
+        {
+            return (Largura * 2) + (Altura * 2);
+        }
+
+        public double Diagonal()
+        {
+            double diagonal = (Largura * Largura) + (Altura * Altura);
+            return Math.Sqrt(diagonal);
+        }
+
+        public override string ToString()
+        {
+            return "Area = " + Area()
+                             + "\nPerimetro = "
+                             + Perimetro()
+                             + "\nDiagonal = "
+                             + Diagonal();
+        }
+    }
+}
