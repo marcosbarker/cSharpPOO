d571be3977530d4a0cc7dd75276c95b8528826e2
Marcos Paulo Marques Correa Gomes <t2m.mpgomes@terceiros.mag.com.br>
2021-12-21 00:07:45 -0300

    code: update ContaBancaria

---
+++ exProposto/ContaBancaria/ContaBancaria/ContaBancaria/ContaMovimentacao.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace ContaBancaria
+{
+    class ContaMovimentacao
+    {
+        public int NumeroConta { get; private set; }
+        public string NomeTitular { get; set; }
+        public double Saldo { get; private set; }
+
+        public ContaMovimentacao(int numeroConta, string nomeTitular)
+        {
+            NumeroConta = numeroConta;
+            NomeTitular = nomeTitular ?? throw new ArgumentNullException(nameof(nomeTitular));
+        }
+
+        public ContaMovimentacao(int numeroConta, string nomeTitular, double saldo) : this(numeroConta, nomeTitular)
+        {
+            Saldo = saldo;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return "Conta " 
+                    + NumeroConta
+                    + ", Titular: "
+                    + NomeTitular
+                    + ", Saldo: $ "
+                    + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ exProposto/ContaBancaria/ContaBancaria/ContaBancaria/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+
+namespace ContaBancaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ContaMovimentacao cm;
+
+            Console.Write("Entre com o número da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre com o titular da conta: ");
+            string titular = Console.ReadLine();
+
+            Console.Write("Havera deposito inicial (s/n): ");
+            char resposta = char.Parse(Console.ReadLine());
+            if (resposta == 's' || resposta == 'S')
+            {
+                Console.WriteLine("Entre com o valor de deposito inicial: ");
+                double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                cm = new ContaMovimentacao(numero, titular, depositoInicial);
+            }
+            else
+            {
+                cm = new ContaMovimentacao(numero, titular);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta: ");
+            Console.WriteLine(cm);
+
+
+            
+        }
+    }
+}
